c360fe2a1d134a9096bfc930984b674f430f3477
nawazkhan <nk12112007nk@gmail.com>
2026-09-07 15:42:00 +0530

    First commit

---
+++ p3.py
@@ -0,0 +1,6 @@
+P = float(input("Enter Principal (P): "))
+R = float(input("Enter Rate of Interest (R): "))
+T = float(input("Enter Time in years (T): "))
+
+SI = (P * R * T) / 100
+print("Simple Interest:", SI)
---
+++ p4.py
@@ -0,0 +1,6 @@
+# Celsius to Fahrenheit
+c = float(input("Enter temperature in Celsius: "))
+f_converted = (c * 9/5) + 32
+print(f"{c}°C is equal to {f_converted}°F")
+
+
